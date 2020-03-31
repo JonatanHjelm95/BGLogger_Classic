976d615b710b5e784a573e3f91a9e3b1a99d201e
JonatanHjelm95 <cph-jh409@cphbusiness.dk>
2020-03-31 11:42:33 +0200

    testing player position

--- Addon/BGLogger_Classic/main.lua
+++ Addon/BGLogger_Classic/main.lua
@@ -3,5 +3,5 @@ Congrats_EventFrame:RegisterEvent("CHAT_MSG_BG_SYSTEM_NEUTRAL")
 Congrats_EventFrame:SetScript("OnEvent",
 	function(self, event, ...)
 		local arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 = ...
-		print('You have entered ' .. arg1 .. '')
+		print(toString(arg1))
 	end)
